ccd3ea068505cd7d42f7b720689a507d89187f43
jlobue <jlobue@eng.ucsd.edu>
2019-09-29 10:31:12 -0700

    update lab 1

---
+++ Labs/lab01/tests/q934.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '9.3.4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> round(sum_of_bills, 2) == 1795730.06
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}

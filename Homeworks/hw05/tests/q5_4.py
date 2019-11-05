87aac1b69dae400c81380a85e0e15e9c742713b5
最美丽 <judyyy@zuimeilis-mbp.home>
2019-11-04 21:00:38 -0800

    Upload hw05

---
+++ Homeworks/hw05/tests/q5_4.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 5_4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(differences, np.ndarray)
+          True
+          >>> len(differences) == 2500
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Homeworks/hw05/tests/q5_5.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 5_5',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(q5_5, int)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
